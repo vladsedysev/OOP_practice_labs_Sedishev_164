5f78167df22eecb9b993fbc2d9b6a47df51b4616
vladsedysev <vladsedysev03@gmail.com>
2025-09-30 22:38:29 +0700

    Update main.cpp

--- Z1/main.cpp
+++ Z1/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Vvedite celoe 4islo: ";
+    int x;
+    cin>>x;
+    if(x%2==0)
+        cout << "Vvedenoe 4islo 4etnoe " <<x;
+    else
+        cout << "Vvedenoe 4islo ne4etnoe " <<x;
+    return 0;
+}
 
